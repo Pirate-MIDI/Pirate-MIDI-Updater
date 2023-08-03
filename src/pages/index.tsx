2f96d469abeab14918ded579047acfd141184bdd
Michael Beckler <beckler@hey.com>
2023-08-02 23:47:28 -0400

    chore: fresh coat of paint for the launch page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,18 +11,31 @@ import pirateMidiImage from '../assets/logo-piratemidi.png'
 
 function App() {
   return (
-    <FadeIn className='w-full h-full overflow-hidden'>
-      <div className='flex flex-col items-center justify-between w-full mt-24 text-center'>
-        <h2 className='text-xl font-semibold'>Connect one of the following devices to get started:</h2>
-        <p>Click a device to learn more about Pirate MIDI&apos;s products.</p>
-        <div className='flex justify-between'>
+    <FadeIn className='w-full h-screen overflow-hidden' childClassName='h-2/6'>
+      <div className='flex justify-end'>
+        <Image
+          width={125}
+          height={133}
+          src={pirateMidiImage}
+          alt='Pirate MIDI Logo'
+        />
+      </div>
+      <div className='flex flex-col items-center'>
+        <h2 className='text-3xl font-semibold'>Connect your device to get started</h2>
+        <span className='m-4'>
+          <ProgressBar size={75} progress={50} label={'Listening...'} spinnerMode={true} trackWidth={5} indicatorWidth={5} />
+        </span>
+      </div>
+      <div className='flex flex-col items-center bg-gray-300 dark:bg-gray-700'>
+        <p className='absolute pt-5 text-sm italic'>Click a device to learn more about Pirate MIDI&apos;s products.</p>
+        <div className='flex justify-between pt-3'>
           <span className='clickable-image'>
             <a href='https://piratemidi.com/pages/bridge-6' target='_blank' rel="noreferrer">
               <Image
-                width={835}
-                height={500}
+                width={400}
+                height={300}
                 src={bridge6Icon}
-                className='!p-10 image-lift'
+                className='!p-12 image-lift'
                 alt='Bridge6 Image'
               />
             </a>
@@ -30,10 +43,10 @@ function App() {
           <span className='clickable-image'>
             <a href='https://piratemidi.com/pages/bridge4' target='_blank' rel="noreferrer">
               <Image
-                width={835}
-                height={500}
+                width={400}
+                height={300}
                 src={bridge4Icon}
-                className='!p-10 image-lift'
+                className='!p-12 image-lift'
                 alt='Bridge4 Image'
               />
             </a>
@@ -41,10 +54,10 @@ function App() {
           <span className='clickable-image'>
             <a href='https://piratemidi.com/products/click-midi-interface-relay-switcher' target='_blank' rel="noreferrer">
               <Image
-                width={835}
-                height={500}
+                width={400}
+                height={300}
                 src={clickIcon}
-                className='!p-10 image-lift'
+                className='!p-12 image-lift'
                 alt='CLiCK Image'
               />
             </a>
@@ -52,8 +65,8 @@ function App() {
           <span className='clickable-image'>
             <a href='https://piratemidi.com/products/%C2%B5loop-4-ch-bypass-and-midi-interface' target='_blank' rel="noreferrer">
               <Image
-                width={835}
-                height={500}
+                width={400}
+                height={300}
                 src={uloopIcon}
                 className='!p-12 image-lift'
                 alt='uLOOP Image'
@@ -61,18 +74,6 @@ function App() {
             </a>
           </span>
         </div>
-        <span className='flex items-center mt-4'>
-          <ProgressBar size={50} progress={5} label={'Waiting for device...'} spinnerMode={true} trackWidth={5} indicatorWidth={5} />
-          <span className='ml-4'>Listening for a device...</span>
-        </span>
-        <span className='mt-4'>
-          <Image
-            width={200}
-            height={133}
-            src={pirateMidiImage}
-            alt='Pirate MIDI Logo'
-          />
-        </span>
       </div>
     </FadeIn >
   )
